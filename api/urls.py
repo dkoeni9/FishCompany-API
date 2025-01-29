390a66d9b7097f4481f40d4e7edb4723fd697f94
Daniil Stenkin <stenkin.d.s@gmail.com>
2025-01-30 00:12:25 +0300

    added register_fisher method

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     # path("", views.index, name="index"),
     path("auth/", include("rest_framework.urls")),
+    path("Auth/RegisterFisher", views.register_fisher),
     path("Admin/AddCompany", views.add_company),
     path("Admin/GetCompanies", views.get_companies),
     path("Auth/SignIn", views.sign_in),
