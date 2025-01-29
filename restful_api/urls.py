44593ac22b84837da52a2f4053b8fed4f0859912
kyoni9d <kyoni9d@gmail.com>
2025-01-29 22:26:27 +0300

    Add debug.default_urlconf

--- restful_api/urls.py
+++ restful_api/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views import debug
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+    path("", debug.default_urlconf),
 ]
