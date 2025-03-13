9dda78f3aa2ce7afb6f02a240bbcab282e2d76eb
koeni9 <kyoni9d@gmail.com>
2025-03-13 03:59:21 +0300

    Add all fields

--- api/serializers.py
+++ api/serializers.py
@@ -5,13 +5,16 @@ from rest_framework import serializers
 class FishSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fish
+        fields = "__all__"
 
 
 class FishBaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = FishBase
+        fields = "__all__"
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = "__all__"
