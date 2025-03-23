0f329c42a40bba5c5fcb822224791da0f0d7d749
koeni9 <kyoni9d@gmail.com>
2025-03-23 05:02:18 +0300

    Register Fish and FishBase

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import Fish, FishBase, User
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Fish)
+admin.site.register(FishBase)
